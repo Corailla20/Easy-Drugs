71a07287b8b0c0ab0434ae6f850009bcdf84a50e
Pierre CHARLES <pier.charles@hotmail.fr>
2016-12-23 15:12:52 +0100

    Add barcode scanner plug-in, generate platforms and implements a simply barcode scan

---
+++ platforms/ios/www/js/controller/startController.js
@@ -0,0 +1,12 @@
+/*
+ File : startController.js
+ The HomeController interact with the home view
+ */
+
+'Use Strict';
+
+angular.module('App')
+
+    .controller('StartController', function($scope) {
+
+    });
