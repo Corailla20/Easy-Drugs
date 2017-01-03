c069da4cbf7034a45d81e39f516a375ad63e8491
Pierre CHARLES <pier.charles@hotmail.fr>
2017-01-03 15:26:44 +0100

    Add SQLITE plugin and create database service file

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -6,13 +6,21 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.barcodeScanner"
         ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-compat": "1.1.0",
-    "phonegap-plugin-barcodescanner": "6.0.5"
+    "phonegap-plugin-barcodescanner": "6.0.5",
+    "cordova-sqlite-storage": "1.5.1"
 };
 // BOTTOM OF METADATA
 });
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -7,13 +7,22 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.barcodeScanner"
         ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-compat": "1.1.0",
-    "phonegap-plugin-barcodescanner": "6.0.5"
+    "phonegap-plugin-barcodescanner": "6.0.5",
+    "cordova-sqlite-storage": "1.5.1"
 }
 // BOTTOM OF METADATA
 });
