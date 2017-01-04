19ae9b4245d1201c3094ad8f93f059713f45c2e4
Pierre CHARLES <pier.charles@hotmail.fr>
2017-01-04 16:23:13 +0100

    Refactoring plugins

--- www/js/controller/textboxController.js
+++ www/js/controller/textboxController.js
@@ -9,75 +9,46 @@ angular.module('App')
 
     .controller('TextboxController', function($scope, $cordovaCamera) {
 
-
-        var options = {
-            quality: 75,
-            destinationType: Camera.DestinationType.DATA_URL,
-            allowEdit: true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
-
         $scope.takePhoto = function () {
-
-            options.sourceType = Camera.PictureSourceType.CAMERA;
-
+            var options = {
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 300,
+                targetHeight: 300,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            };
             $cordovaCamera.getPicture(options).then(function (imageData) {
-                $scope.image = "data:image/jpeg;base64," + imageData;
-                $scope.text = null;
-
-                $timeout(function() {
-
-                    TesseractPlugin.loadLanguage(language, function(response) {
-                        deferred.resolve(response);
-                    }, function(reason) {
-                        deferred.reject('Error on loading OCR file for your language. ' + reason);
-                    });
-
-                    TesseractPlugin.recognizeText(imageData, language, function(recognizedText) {
-                        $scope.text = recognizedText;
-                    }, function(reason) {
-                        console.log('Error on recognizing text from image. ' + reason);
-                    });
-
-                });
-
+                $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                alert(imageData);
 
             }, function (err) {
-                console.log('ERROR with camera plugin. Error: ' + err);
+                alert("An error occured. Show a message to the user"+err);
             });
         };
 
         $scope.choosePhoto = function () {
-
-            options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
-
+            var options = {
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 300,
+                targetHeight: 300,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            };
 
             $cordovaCamera.getPicture(options).then(function (imageData) {
-                $scope.image = "data:image/jpeg;base64," + imageData;
-                $scope.text = null;
-
-                $timeout(function() {
-
-                    TesseractPlugin.loadLanguage(language, function(response) {
-                        deferred.resolve(response);
-                    }, function(reason) {
-                        deferred.reject('Error on loading OCR file for your language. ' + reason);
-                    });
-
-                    TesseractPlugin.recognizeText(imageData, language, function(recognizedText) {
-                        $scope.text = recognizedText;
-                    }, function(reason) {
-                        console.log('Error on recognizing text from image. ' + reason);
-                    });
-
-                });
+                $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                alert(imageData);
 
             }, function (err) {
-                console.log('ERROR with camera plugin. Error: ' + err);
+                alert("An error occured. Show a message to the user"+err);
             });
         };
 
