185e9a9f1d62665b92948b115b1fe5f49c824af9
febraiz <deathpote@hotmail.fr>
2017-01-05 17:14:37 +0100

    Updated OCR and add commentarys

--- www/js/controller/textboxController.js
+++ www/js/controller/textboxController.js
@@ -9,13 +9,6 @@ angular.module('App')
 
     .controller('TextboxController', function($scope, $cordovaCamera) {
 
-
-        TesseractPlugin.loadLanguage(language, function(response) {
-            deferred.resolve(response);
-        }, function(reason) {
-            deferred.reject('Error on loading OCR file for your language. ' + reason);
-        });
-
         $scope.takePhoto = function () {
             var options = {
                 quality: 75,
@@ -29,12 +22,13 @@ angular.module('App')
                 saveToPhotoAlbum: false
             };
             $cordovaCamera.getPicture(options).then(function (imageData) {
-                $scope.imgURI = "data:image/jpeg;base64," + imageData;
 
-                TesseractPlugin.recognizeText(imageData, language, function(recognizedText) {
-                    $scope.text = recognizedText;
-                }, function(reason) {
-                    alert('Error on recognizing text from image. ' + reason);
+                //http://tesseract.projectnaptha.com/
+
+                var imgURI = "data:image/jpeg;base64," + imageData;
+
+                Tesseract.recognize(imgURI).then(function(result){
+                    alert(result);
                 });
 
             }, function (err) {
@@ -59,11 +53,7 @@ angular.module('App')
             $cordovaCamera.getPicture(options).then(function (imageData) {
                 $scope.imgURI = "data:image/jpeg;base64," + imageData;
                 
-                TesseractPlugin.recognizeText(imageData, language, function(recognizedText) {
-                    $scope.text = recognizedText;
-                }, function(reason) {
-                    alert('Error on recognizing text from image. ' + reason);
-                });
+
 
             }, function (err) {
                 alert("An error occured. Show a message to the user"+err);
