e60e49ec7a521443bfa2aaae4cf7463fc4a8fc43
Pierre CHARLES <pier.charles@hotmail.fr>
2017-01-05 17:38:26 +0100

    Add some change on web_service, it's working

--- web_service/Config/config.php
+++ web_service/Config/config.php
@@ -2,7 +2,7 @@
 
     //Varrible d'identifications à la base de données :
 
-    $base="u692460414_easyd";
-    $login="u692460414_easyd";
+    $base="id472023_easydrugs";
+    $login="id472023_easydrugs";
     $mdp="master&643";
-    $host="mysql.hostinger.fr";
+    $host="localhost";
--- web_service/index.php
+++ web_service/index.php
@@ -1,11 +1,29 @@
 <?php
 
 //chargement config
-require_once(__DIR__ . 'Config/config.php');
+require_once(__DIR__ . '/Config/config.php');
 
+require_once(__DIR__ . '/Config/config.php');
+require_once(__DIR__ . '/Model/Entities/Drug.php');
+require_once(__DIR__ . '/Model/Entities/Drugs.php');
+require_once(__DIR__ . '/Model/Entities/User.php');
+require_once(__DIR__ . '/Model/DBConnexion.php');
+require_once(__DIR__ . '/Request/Get.php');
 
-if ($_SERVER['GET_DRUGS'] === 'GET') {
-    // The request is using the POST method
+use Model\DBConnexion;
+use Model\Entities\Drug;
+use Model\Entities\Drugs;
+
+$request = "SELECT * FROM DRUG";
+DBConnexion::getInstance()->prepareAndExecuterQuerySelect($request, null);
+$response = DBConnexion::getInstance()->getResult();
+$drugs = new Drugs();
+foreach ($response as $resp)
+{
+    $drug = new Drug($resp['id_drug'], $resp['name'], $resp['sub_name'], $resp['bar_code'], $resp['flash_code'], $resp['notice']);
+    $drugs->addDrug($drug->toJson());
 }
 
+DBConnexion::getInstance()->destroyQueryResults();
 
+echo $drugs->getDrugsToJson();
